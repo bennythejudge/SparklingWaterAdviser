19e1285740a621ba3e20dc63d19a9b7245b69698
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-11-20 23:56:11 +0100

    Initial commit

---
+++ app/src/main/java/android/benedetto/com/sparklingwateradviser/FindSparklingWaterActivity.java
@@ -0,0 +1,17 @@
+package android.benedetto.com.sparklingwateradviser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FindSparklingWaterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_sparkling_water);
+    }
+
+    public void onClickFindSparklingWater(View view) {
+    }
+}
